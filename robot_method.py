645129b077c86d571cad160d34e2ac3487ff7205
Stefan Golas <stefanmgolas@gmail.com>
2023-01-24 23:37:21 -0500

    added functions

--- robot_method.py
+++ robot_method.py
@@ -8,7 +8,7 @@ import os
 from pyhamilton import (HamiltonInterface,  LayoutManager, 
  Plate96, Tip96, initialize, tip_pick_up, tip_eject, 
  aspirate, dispense,  oemerr, resource_list_with_prefix, normal_logging,
- odtc_connect, odtc_reset, odtc_initialize)
+ odtc_connect, odtc_reset, odtc_initialize, odtc_get_status, odtc_download_protocol)
 
 liq_class = 'StandardVolumeFilter_Water_DispenseJet_Empty'
 
@@ -23,6 +23,10 @@ if __name__ == '__main__':
     with HamiltonInterface(simulate=True) as ham_int:
         normal_logging(ham_int, os.getcwd())
         initialize(ham_int)
-        odtc_id = odtc_connect(ham_int, "192.168.1.2", "192.168.1.50", "", True)
-        odtc_reset(ham_int, odtc_id, "", True, 120, "", "")
-        odtc_initialize(ham_int, odtc_id, "")
+        
+        odtc_id = odtc_connect(ham_int, local_ip = "192.168.1.2", device_ip = "192.168.1.50", simulation_mode = True)
+        odtc_reset(ham_int, odtc_id, simulation_mode = True, timeout = 120)
+        odtc_initialize(ham_int, odtc_id)
+        odtc_get_status(ham_int, odtc_id) # Doesn't return anything in simulation mode
+        
+        odtc_download_protocol(ham_int, odtc_id, protocol_file = 'abc.xml')
